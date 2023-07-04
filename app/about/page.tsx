2d7b289da6c5e107d6d445c5145d069ff5cd9663
vhazbun <vhazbun@getaroom.com>
2023-07-03 20:45:04 -0500

    Style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,17 @@
 import { Metadata } from 'next';
 
-export const dynamic = 'force-dynamic'; // no necessary, just for demonstration
+export const dynamic = 'force-static'; // no necessary, just for demonstration
 
 export const metadata: Metadata = {
   title: 'About Us',
-  description: 'About Us',
+  description: 'About NextSpace',
 };
 
 export default function Blog() {
   return (
     <div>
       <h1>About us</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>We are a social media company that wants to bring people together!</p>
     </div>
   );
 }
